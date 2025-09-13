1d8addcb5322d40456a685823dcf85a3a1e01d62
Angelo <48489278+conhlee@users.noreply.github.com>
2025-09-14 00:25:33 +0200

    v1.1: speed ctrl, optimizations, refactoring, & more!

--- SRC/main.c
+++ SRC/main.c
@@ -1,5 +1,6 @@
 /*
-    *   Original C 'boing' reconstruction by Jimmy Maher in 2009-2010
+    *   Original 'boing' demo by Dale Luck and R.J. Mical in 1984-1985
+    *   C 'boing' reconstruction by Jimmy Maher in 2009-2010
     *   TI-84 Plus CE-T port by conhlee in 2025
 */
 
@@ -11,16 +12,30 @@
 #include "BALL.IND.inc"
 #include "BALL.PAL.inc"
 
+/* Fixed-point macros. */
+#define FIX_SHIFT   (5)
+#define FIX_SCALE   (1 << FIX_SHIFT)
+
+#define TO_FIXED(x)     ((x) * FIX_SCALE)
+#define FROM_FIXED(x)   ((x) / FIX_SCALE)
+
+#define FIXED_MULT(x, y) ((x) * (y) / FIX_SCALE)
+
+#define IS_FIXED_WHOLE(x) (((x) & (FIX_SCALE - 1)) == 0)
+
+/* Ball image dimensions. */
 #define BALL_WIDTH (144)
 #define BALL_HEIGHT (100)
 
-// The frame rate is a little slow, so we ramp up the speed to 2x to account for it.
-#define ANIME_SPEED (2)
+// The frame rate is a little slow, so we ramp up the base speed to 2x to account for it.
+#define BALL_BASE_SPEED (2)
 
 typedef struct {
-    short posX, posY;
-    short scrollX, scrollY;
-    short colorCycle;
+    // All of these values are fixed-point.
+    int posX, posY;
+    int scrollX, scrollY;
+    int colorCycle;
+    int animSpeed;
 
     union {
         gfx_sprite_t spr;
@@ -28,6 +43,18 @@ typedef struct {
     };
 } BallState;
 static BallState sBall;
+
+static inline void gfxInit(void) {
+    gfx_Begin();
+    gfx_SetDrawBuffer();
+
+    gfx_SetPalette(BALL_PAL, BALL_PAL_len, 0);
+    gfx_SetTransparentColor(0);
+}
+
+static inline void gfxShutdown(void) {
+    gfx_End();
+}
 
 static inline void ballInit(void) {
     sBall.spr.width = BALL_WIDTH;
@@ -38,100 +65,115 @@ static inline void ballInit(void) {
         sBall.spr.data[i] = BALL_IND[i];
     }
 
-    sBall.posX = 0;
-    sBall.posY = 0;
-    sBall.scrollX = 1;
-    sBall.scrollY = -1;
-    sBall.colorCycle = 2;
+    sBall.posX = TO_FIXED(0);
+    sBall.posY = TO_FIXED(0);
+    sBall.scrollX = TO_FIXED(1);
+    sBall.scrollY = TO_FIXED(-1);
+    sBall.colorCycle = TO_FIXED(2);
+
+    sBall.animSpeed = TO_FIXED(1);
 }
 
 static inline void ballUpdate(void) {
-    // Offset by one to skip transparent color at index 0.
-    uint16_t *palette = ((uint16_t *)BALL_PAL) + 1;
+    // Writing directly to gfx_palette .. kind of a dirty hack..
+    uint16_t *palette = (uint16_t *)(gfx_palette) + 1;
 
-    sBall.colorCycle += ((sBall.scrollX > 0) ? -1 : 1) * ANIME_SPEED;
+    sBall.colorCycle += FIXED_MULT(
+        TO_FIXED((sBall.scrollX > 0) ? -1 : 1),
+        sBall.animSpeed * BALL_BASE_SPEED
+    );
 
     if (sBall.colorCycle < 0)
-        sBall.colorCycle = 13;
-    else if (sBall.colorCycle > 13)
+        sBall.colorCycle = TO_FIXED(13);
+    else if (sBall.colorCycle > TO_FIXED(13))
         sBall.colorCycle = 0;
 
+    short colorCycleInt = FROM_FIXED(sBall.colorCycle);
+
     for (int i = 0; i < 7; i++) {
-        if ((sBall.colorCycle + i) < 14) {
-            palette[sBall.colorCycle + i + 2] = gfx_RGBTo1555(255, 255, 255);
-            palette[sBall.colorCycle + i + 18] = gfx_RGBTo1555(255, 255, 255);
+        if ((colorCycleInt + i) < 14) {
+            palette[colorCycleInt + i + 2] = gfx_RGBTo1555(255, 255, 255);
+            palette[colorCycleInt + i + 18] = gfx_RGBTo1555(255, 255, 255);
         }
         else {
-            palette[sBall.colorCycle + i - 12] = gfx_RGBTo1555(255, 255, 255);
-            palette[sBall.colorCycle + i + 4] = gfx_RGBTo1555(255, 255, 255);
+            palette[colorCycleInt + i - 12] = gfx_RGBTo1555(255, 255, 255);
+            palette[colorCycleInt + i + 4] = gfx_RGBTo1555(255, 255, 255);
         }
     }
     for (int i = 7; i < 14; i++) {
-        if ((sBall.colorCycle + i) < 14) {
-            palette[sBall.colorCycle + i + 2] = gfx_RGBTo1555(255, 0, 0);
-            palette[sBall.colorCycle + i + 18] = gfx_RGBTo1555(255, 0, 0);
+        if ((colorCycleInt + i) < 14) {
+            palette[colorCycleInt + i + 2] = gfx_RGBTo1555(255, 0, 0);
+            palette[colorCycleInt + i + 18] = gfx_RGBTo1555(255, 0, 0);
         }
         else {
-            palette[sBall.colorCycle + i - 12] = gfx_RGBTo1555(255, 0, 0);
-            palette[sBall.colorCycle + i + 4] = gfx_RGBTo1555(255, 0, 0);
+            palette[colorCycleInt + i - 12] = gfx_RGBTo1555(255, 0, 0);
+            palette[colorCycleInt + i + 4] = gfx_RGBTo1555(255, 0, 0);
         }
     }
 
     if (sBall.scrollX > 0) {
-        palette[sBall.colorCycle + 2] = gfx_RGBTo1555(255, 221, 221);
-        palette[sBall.colorCycle + 18] = gfx_RGBTo1555(255, 221, 221);
+        palette[colorCycleInt + 2] = gfx_RGBTo1555(255, 221, 221);
+        palette[colorCycleInt + 18] = gfx_RGBTo1555(255, 221, 221);
     }
     else {
-        if ((sBall.colorCycle + 6) < 14) {
-            palette[sBall.colorCycle + 8] = gfx_RGBTo1555(255, 221, 221);
-            palette[sBall.colorCycle + 24] = gfx_RGBTo1555(255, 221, 221);
+        if ((colorCycleInt + 6) < 14) {
+            palette[colorCycleInt + 8] = gfx_RGBTo1555(255, 221, 221);
+            palette[colorCycleInt + 24] = gfx_RGBTo1555(255, 221, 221);
         }
         else {
-            palette[sBall.colorCycle - 6] = gfx_RGBTo1555(255, 221, 221);
-            palette[sBall.colorCycle + 10] = gfx_RGBTo1555(255, 221, 221);
+            palette[colorCycleInt - 6] = gfx_RGBTo1555(255, 221, 221);
+            palette[colorCycleInt + 10] = gfx_RGBTo1555(255, 221, 221);
         }
     }
 
-    sBall.posX += sBall.scrollX * ANIME_SPEED;
-    if (sBall.posX <= -55 || sBall.posX >= 100)
+    sBall.posX += FIXED_MULT(sBall.scrollX, sBall.animSpeed * BALL_BASE_SPEED);
+    if (sBall.posX <= TO_FIXED(-55)) {
         sBall.scrollX = -sBall.scrollX;
+        sBall.posX = TO_FIXED(-55 + 1);
+    }
+    if (sBall.posX >= TO_FIXED(100)) {
+        sBall.scrollX = -sBall.scrollX;
+        sBall.posX = TO_FIXED(100 - 1);
+    }
 
-    short scrollYFact;
-    if (sBall.posY > -10)
+    int scrollYFact;
+    if (sBall.posY > TO_FIXED(-10))
         scrollYFact = 1;
-    else if (sBall.posY > -30)
+    else if (sBall.posY > TO_FIXED(-30))
         scrollYFact = 2;
-    else if (sBall.posY > -60)
+    else if (sBall.posY > TO_FIXED(-60))
         scrollYFact = 3;
     else
         scrollYFact = 4;
 
-    sBall.posY += sBall.scrollY * scrollYFact * ANIME_SPEED;
+    sBall.posY += FIXED_MULT(sBall.scrollY * scrollYFact, sBall.animSpeed * BALL_BASE_SPEED);
 
-    if (sBall.posY <= -100 || sBall.posY >= 0)
+    if (sBall.posY <= TO_FIXED(-100)) {
         sBall.scrollY = -sBall.scrollY;
+        sBall.posY = TO_FIXED(-100 + 1);
+    }
+    if (sBall.posY >= TO_FIXED(0)) {
+        sBall.scrollY = -sBall.scrollY;
+        sBall.posY = TO_FIXED(0 - 1);
+    }
 }
 
 static inline void ballDraw(void) {
-    int x = (BALL_WIDTH / 2) + sBall.posX;
-    int y = (BALL_HEIGHT / 2) - sBall.posY - 20;
+    int x = (BALL_WIDTH / 2) + FROM_FIXED(sBall.posX);
+    int y = (BALL_HEIGHT / 2) - FROM_FIXED(sBall.posY) - 20;
 
     gfx_TransparentSprite(&sBall.spr, x, y);
 }
 
 static inline void bgDraw(void) {
-    // Draw the gray BG
+    // Draw the gray BG.
     gfx_FillScreen(1);
 
     // Assign the purple color ..
     gfx_SetColor(17);
 
-    /*
-        TODO: is it possible to buffer these lines out to a texture for a speed boost?
-        I'm fairly sure drawing all these lines is a little slow ..
-    */
-
 // Scale AMIGA coords to our screen coords.
+// All of the loops below are unrolled, so this math is never done in realtime; neat!
 #define SCALE_X(x) ((int)((int)(x) * (GFX_LCD_WIDTH) / 336))
 #define SCALE_Y(y) ((int)((int)(y) * (GFX_LCD_HEIGHT) / 216))
 
@@ -166,29 +208,121 @@ static inline void bgDraw(void) {
 
 }
 
-static inline void syncPalette(void) {
-    gfx_SetPalette(BALL_PAL, BALL_PAL_len, 0);
-    gfx_SetTransparentColor(0);
+static unsigned int sSpeedMesgTimer;
+static unsigned int sExitMesgTimer;
+
+// We keep these times low .. displaying text is pretty slow!
+#define SHOW_SPEED_MESG() do { sSpeedMesgTimer = 20; } while (0)
+#define SHOW_EXIT_MESG() do { sExitMesgTimer = 20; } while (0)
+
+static inline void mesgInit(void) {
+    gfx_SetTextFGColor(17);
+    gfx_SetTextBGColor(1);
+    gfx_SetTextTransparentColor(0);
+
+    gfx_SetTextConfig(gfx_text_noclip);
+    gfx_SetMonospaceFont(8);
+
+    SHOW_SPEED_MESG();
+    SHOW_EXIT_MESG();
+}
+
+static inline void mesgDraw(void) {
+    int showSpeedMesg = sSpeedMesgTimer > 0;
+    if (showSpeedMesg) {
+        gfx_SetTextXY(10, 10);
+        
+        gfx_PrintString("SPEED : ");
+
+        int animSpeedFix = sBall.animSpeed;
+        int animSpeedInt = FROM_FIXED(animSpeedFix);
+
+        if (IS_FIXED_WHOLE(animSpeedFix)) {
+            // Hooray! No slow math for us!
+            gfx_PrintInt(animSpeedInt, 1);
+            gfx_PrintString(".000");
+        }
+        else {
+            int fracPart = ((animSpeedFix & (FIX_SCALE - 1)) * 1000) / TO_FIXED(1);
+
+            if (animSpeedFix < 0 && animSpeedInt == 0) {
+                gfx_PrintChar('-'); // We have to manually add the - in this case.
+            }
+            gfx_PrintInt(animSpeedInt, 1);
+            gfx_PrintChar('.');
+            gfx_PrintInt(fracPart, 3);
+        }
+
+        gfx_PrintString(" ( use arrow keys )");
+
+        sSpeedMesgTimer--;
+    }
+
+    if (sExitMesgTimer > 0) {
+        gfx_PrintStringXY("press MODE to exit", 10, showSpeedMesg ? 20 : 10);
+
+        sExitMesgTimer--;
+    }
+}
+
+static inline uint8_t keyUpdate(void) {
+    uint8_t keyCode = os_GetCSC();
+
+    switch (keyCode) {
+    case 0:
+    case sk_Mode:
+        break;
+
+    case sk_Left:
+        sBall.animSpeed -= TO_FIXED(1) / 8;
+        SHOW_SPEED_MESG();
+        break;
+    case sk_Right:
+        sBall.animSpeed += TO_FIXED(1) / 8;
+        SHOW_SPEED_MESG();
+        break;
+    case sk_Up:
+        sBall.animSpeed += TO_FIXED(1);
+        SHOW_SPEED_MESG();
+        break;
+    case sk_Down:
+        sBall.animSpeed -= TO_FIXED(1);
+        SHOW_SPEED_MESG();
+        break;
+
+    default:
+        SHOW_EXIT_MESG();
+        break;
+    }
+
+    return keyCode;
 }
 
 int main(void) {
+    gfxInit();
+
     ballInit();
 
-    gfx_Begin();
-    gfx_SetDrawBuffer();
+    mesgInit();
 
-    do {
+    for (;;) {
+        uint8_t keyCode = keyUpdate();
+
         ballUpdate();
-
-        syncPalette();
 
         bgDraw();
         ballDraw();
 
-        gfx_SwapDraw();
-    } while (os_GetCSC() == 0);
+        mesgDraw();
 
-    gfx_End();
+        gfx_SwapDraw();
+
+        if (keyCode == sk_Mode) {
+            break;
+        }
+    }
+
+    gfxShutdown();
 
     return 0;
 }
